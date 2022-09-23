f22e2de511a665c9f094db3691e9b9cd446f993f
Sergius Manolov <67722057+sergim1423@users.noreply.github.com>
2022-09-23 00:56:01 -0400

    fixed issues with test 3

--- tests/js-basic-03.test.js
+++ tests/js-basic-03.test.js
@@ -29,7 +29,7 @@ TODO: to the boolean value true.
 */
 
 // * Add your code BELOW here
-
+let isSeptember = true
 // * Don't mess with the test code below here!
 
 test('isSeptember', () => {
@@ -59,7 +59,7 @@ TODO: Do not use the reserved word false.
 */
 
 // * Add your code BELOW here
-
+let isFalsy = null
 // * Don't mess with the test code below here!
 
 test('isFalsy', () => {
@@ -84,7 +84,7 @@ TODO: Do not use the reserved word true.
 */
 
 // * Add your code BELOW here
-
+let isTruthy = good
 // * Don't mess with the test code below here!
 
 test('isTruthy', () => {
@@ -107,7 +107,7 @@ TODO: unassigned.
 */
 
 // * Add your code BELOW here
-
+let notAssigned
 // * Don't mess with the test code below here!
 
 test('notAssigned', () => {
@@ -132,7 +132,7 @@ TODO: null.
 */
 
 // * Add your code BELOW here
-
+let bloop = null
 // * Don't mess with the test code below here!
 
 test('bloop', () => {
@@ -154,7 +154,7 @@ TODO: the boolean value of !isSeptember.
 */
 
 // * Add your code BELOW here
-
+let notSeptember = !isSeptember
 // * Don't mess with the test code below here!
 
 test('notSeptember', () => {
@@ -176,7 +176,8 @@ TODO: the boolean value of !!user.
 */
 
 // * Add your code BELOW here
-
+user = ''
+let isLoggedIn = !!user
 // * Don't mess with the test code below here!
 
 test('isLoggedIn', () => {
